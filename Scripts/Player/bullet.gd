68598298af34a390cec1327ca13c28abf62c34ea
taldeam <taldea.dev@gmail.com>
2024-08-26 21:26:20 +0200

    muchas cosas

---
+++ Scripts/Player/bullet.gd
@@ -0,0 +1,24 @@
+extends CharacterBody2D
+class_name Bullet
+
+const SPEED: float = 300.0
+const DAMAGE: int = 1
+var direction: Vector2 = Vector2.ZERO
+
+func _ready() -> void:
+	# Configurar la rotación inicial de la bala
+	rotation = direction.angle()
+	remove_bullet()
+	
+func _physics_process(delta: float) -> void:
+	# Mover la bala en la dirección configurada
+	velocity = direction * SPEED
+	move_and_slide()
+	
+func remove_bullet():
+	var tween = get_tree().create_tween()
+	tween.tween_property(self, "scale", Vector2(), 3)
+	tween.tween_callback(self.queue_free)
+	
+func set_direction(dir: Vector2) -> void:
+	direction = dir.normalized()
--- Scripts/Player/nave_principal.gd
+++ Scripts/Player/nave_principal.gd
@@ -1,10 +1,11 @@
 extends CharacterBody2D
 
 @export var SPEED = 400.0
-@onready var joystick_left : VirtualJoystick = $"../Test/UI/Virtual joystick left"
-@onready var joystick_right : VirtualJoystick = $"../Test/UI/Virtual joystick right"
+@onready var joystick_left : VirtualJoystick = $"../UI2/Virtual joystick left"
+@onready var joystick_right : VirtualJoystick = $"../UI2/Virtual joystick right"
 @onready var sprite : Sprite2D = $Sprite2D
 var move_vector := Vector2.ZERO
+@onready var collisionShape : CollisionShape2D = $CollisionShape2D
 
 func _physics_process(delta: float) -> void:
 	# Movimiento basado en el joystick izquierdo
@@ -20,5 +21,21 @@ func _physics_process(delta: float) -> void:
 	# Rotation:
 	if joystick_right and joystick_right.is_pressed:
 		sprite.rotation = joystick_right.output.angle()
-		
+	
+	_state_shoot1()
 	move_and_slide()
+
+func _state_shoot1():
+	# Cargar la escena de la bala (Bullet)
+	var bullet_scene = preload("res://escenas/bullet.tscn")
+	# Instanciar la bala
+	var bullet_instance = bullet_scene.instantiate()
+	# Configurar la posición de la bala en la posición del HitBoxComponent
+	bullet_instance.position = self.position
+	
+	# Obtener la dirección hacia donde mira el personaje usando is_rotate
+	var direction = Vector2.RIGHT
+	# Configurar la dirección de la bala
+	bullet_instance.set_direction(direction)
+	# Agregar la bala a la escena
+	get_parent().add_child(bullet_instance)
