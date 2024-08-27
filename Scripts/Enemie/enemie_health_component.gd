066551a5cc49bd6f23624b3d2c84241e9d9cb7ea
taldeam <taldea.dev@gmail.com>
2024-08-27 20:14:06 +0200

    nave explota

--- Scripts/Enemie/enemie_health_component.gd
+++ Scripts/Enemie/enemie_health_component.gd
@@ -1,7 +1,8 @@
 extends Area2D
 class_name Enemie_health_component
 
-@export var health: int = 2
+@export var health: int = 20
+@onready var particles: CPUParticles2D = $"../CPUParticles2D"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,7 +17,14 @@ func hit(area):
 		health -= area.damage
 		area.bullet_hit(true)
 		check_heal()
+		show_damage_particles()
 
 func check_heal():
 	if health <= 0:
 		get_parent().queue_free()
+
+func show_damage_particles() -> void:
+	particles.emitting = false  # Reinicia la emisión si ya estaba activa
+	particles.one_shot = true   # Asegúrate de que solo se emita una vez
+	particles.emitting = true   # Inicia la emisión de partículas
+	pass
