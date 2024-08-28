5faf21f7497297fb6899fea5cf64b14b4c644576
taldeam <taldea.dev@gmail.com>
2024-08-28 20:21:21 +0200

    add

--- Scripts/Player/nave_principal.gd
+++ Scripts/Player/nave_principal.gd
@@ -37,7 +37,6 @@ func _state_shoot1():
 	# Cargar la escena de la bala (Bullet)
 	var bullet_scene = preload("res://escenas/bullet.tscn")
 	# Instanciar la bala
-	
 	var bullet_instance = bullet_scene.instantiate()
 	# Configurar la posición de la bala en la posición del HitBoxComponent
 	bullet_instance.position = self.position
