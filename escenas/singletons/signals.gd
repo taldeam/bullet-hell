99397e12c72dc1dba78d460290d481977f554793
Tomas Aldea Moral <tomasaldeamoral@MacBook-Air-de-Tomas.local>
2024-09-28 13:54:44 +0200

    textos en inlges

--- escenas/singletons/signals.gd
+++ escenas/singletons/signals.gd
@@ -11,34 +11,34 @@ var enemies_in_group : Array = []
 # cada vez que añada un buff aqui, hacerlo en power_up_item
 var BuffArray : Array = [
 	{
-	"state": false,
-	"buffKey": 0,
-	"buffName": "escudo",
-	"buffLabel": "Añade un escudo que resiste hasta 5 impatos."
+		"state": false,
+		"buffKey": 0,
+		"buffName": "escudo",
+		"buffLabel": "Adds a shield that withstands up to 5 impacts."
 	},
 	{
-	"state": false,
-	"buffKey": 1,
-	"buffName": "laser",
-	"buffLabel": "Añade un laser automático cada 15 segundos."
+		"state": false,
+		"buffKey": 1,
+		"buffName": "laser",
+		"buffLabel": "Adds an automatic laser every 15 seconds."
 	},
 	{
-	"state": false,
-	"buffKey": 2,
-	"buffName": "nave",
-	"buffLabel": "Añade una nave aliada en tu posición."
+		"state": false,
+		"buffKey": 2,
+		"buffName": "nave",
+		"buffLabel": "Adds an allied ship at your position."
 	},
 	{
-	"state": false,
-	"buffKey": 3,
-	"buffName": "attack_speed",
-	"buffLabel": "Aumenta tu velocidad de ataque un 10%."
+		"state": false,
+		"buffKey": 3,
+		"buffName": "attack_speed",
+		"buffLabel": "Increases your attack speed by 10%."
 	},
 	{
-	"state": true,
-	"buffKey": 4,
-	"buffName": "attack_speed_ally",
-	"buffLabel": "Aumenta la velocidad de ataque de las naves aliadas un 10%."
+		"state": true,
+		"buffKey": 4,
+		"buffName": "attack_speed_ally",
+		"buffLabel": "Increases the attack speed of allied ships by 10%."
 	}
 ]
 
